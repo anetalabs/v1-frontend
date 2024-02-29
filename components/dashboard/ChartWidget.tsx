8aa64ae9640c43f929600503fd7e9339b7f3fb7b
tronexis <tronexis>
2024-02-29 13:56:35 +0600

    isolate ChartWidget component

---
+++ components/dashboard/ChartWidget.tsx
@@ -0,0 +1,53 @@
+import { AnetaData } from "../../hooks/useAnetaData";
+import ChartComponent from "./ChartComponent";
+import styles from "../../styles/chartWidget.module.scss";
+
+interface ChartWidgetProps {
+  title: string;
+  amount: number;
+  suffix: string;
+  data?: AnetaData[];
+  hasFilter?: boolean;
+  onFilterBtnClick?: () => void;
+}
+
+const ChartWidget = (props: ChartWidgetProps) => {
+  return (
+    <div className={styles.chartWidget}>
+      <div className={styles.headerChart}>
+        <div className={styles.valuesGroup}>
+          <h2 className={styles.titleChart}>{props.title}</h2>
+          <div className={styles.tokenChart}>
+            <h3 className={styles.tokenTitle}>
+              {props.amount} {props.suffix}
+            </h3>
+          </div>
+        </div>
+        {props.hasFilter && (
+          <div className={styles.btnGroup}>
+            <button className={styles.btnBtc} onClick={props.onFilterBtnClick}>
+              <span className={styles.btnText}>All Time</span>
+              {/* <svg width="12" height="12" id="icon" className={styles.icon}>
+                <use href="/images/icons/arrow-right.svg#icon"></use>
+              </svg> */}
+            </button>
+          </div>
+        )}
+      </div>
+      {props.data ? (
+        <ChartComponent
+          data={props.data}
+          height={70}
+          suffix={props.suffix}
+          hideLegends
+        />
+      ) : (
+        <div className={styles.loaderChart}>
+          <div className={styles.loader}></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ChartWidget;
