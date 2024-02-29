4d5a872a756a31523a9a9822a45b2fc3e11f0c0e
tronexis <tronexis>
2024-02-29 12:48:34 +0600

    update page titles

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -11,6 +11,7 @@ import useLucid from "../../hooks/useLucid";
 import { AnetaData } from "../../hooks/useAnetaData";
 import { Cip30Wallet } from "@cardano-sdk/dapp-connector";
 import ConnectWallet from "../../components/partials/navbar/ConnectWallet";
+import Head from "next/head";
 
 export default function Dashboard() {
   const {
@@ -87,137 +88,144 @@ export default function Dashboard() {
   }, [address]);
 
   return (
-    <section className={styles.dashboardContainer}>
-      <div className={styles.sectionChart}>
-        <div className={styles.chartTvl}>
-          <div className={styles.headerChart}>
-            <div className={styles.valuesGroup}>
-              <h2 className={styles.titleChart}>TVL</h2>
-              <div className={styles.tokenChart}>
-                <svg width="32" height="32" id="icon">
-                  <use href="/images/crypto/cbtc-logo.svg#Layer_1"></use>
-                </svg>
-                {loading ? (
-                  <div className={styles.value}>
-                    <div className={styles.loader}></div>
-                  </div>
-                ) : (
-                  data && (
-                    <p className={styles.value}>
-                      {numberToFixed(data.quantity)}
-                    </p>
-                  )
-                )}
-                <h3 className={styles.tokenTitle}>cBTC</h3>
+    <>
+      <Head>
+        <title>Dashboard | anetaBTC</title>
+      </Head>
+      <section className={styles.dashboardContainer}>
+        <div className={styles.sectionChart}>
+          <div className={styles.chartTvl}>
+            <div className={styles.headerChart}>
+              <div className={styles.valuesGroup}>
+                <h2 className={styles.titleChart}>TVL</h2>
+                <div className={styles.tokenChart}>
+                  <svg width="32" height="32" id="icon">
+                    <use href="/images/crypto/cbtc-logo.svg#Layer_1"></use>
+                  </svg>
+                  {loading ? (
+                    <div className={styles.value}>
+                      <div className={styles.loader}></div>
+                    </div>
+                  ) : (
+                    data && (
+                      <p className={styles.value}>
+                        {numberToFixed(data.quantity)}
+                      </p>
+                    )
+                  )}
+                  <h3 className={styles.tokenTitle}>cBTC</h3>
+                </div>
+                <p>{formattedDate}</p>
               </div>
-              <p>{formattedDate}</p>
+              <div className={styles.btnGroup}>
+                <Link
+                  href={vaultBtc}
+                  className={styles.btnBtc}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className={styles.btnText}>View BTC Vaults</p>
+                  <svg width="12" height="12" id="icon" className={styles.icon}>
+                    <use href="/images/icons/arrow-right.svg#icon"></use>
+                  </svg>
+                </Link>
+                <Link
+                  href={linkcBtc}
+                  className={styles.btncBtc}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <p className={styles.btnText}>View cBTC Token</p>
+                  <svg width="12" height="12" id="icon" className={styles.icon}>
+                    <use href="/images/icons/arrow-right.svg#icon"></use>
+                  </svg>
+                </Link>
+              </div>
             </div>
-            <div className={styles.btnGroup}>
-              <Link
-                href={vaultBtc}
-                className={styles.btnBtc}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <p className={styles.btnText}>View BTC Vaults</p>
-                <svg width="12" height="12" id="icon" className={styles.icon}>
-                  <use href="/images/icons/arrow-right.svg#icon"></use>
-                </svg>
-              </Link>
-              <Link
-                href={linkcBtc}
-                className={styles.btncBtc}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <p className={styles.btnText}>View cBTC Token</p>
-                <svg width="12" height="12" id="icon" className={styles.icon}>
-                  <use href="/images/icons/arrow-right.svg#icon"></use>
-                </svg>
-              </Link>
-            </div>
+            {tvlData ? (
+              <ChartComponent data={tvlData} />
+            ) : (
+              <div className={styles.loaderChart}>
+                <div className={styles.loader}></div>
+              </div>
+            )}
           </div>
-          {tvlData ? (
-            <ChartComponent data={tvlData} />
-          ) : (
-            <div className={styles.loaderChart}>
-              <div className={styles.loader}></div>
-            </div>
-          )}
+          <ChartWidget
+            title="Protocol Volume"
+            amount={4567}
+            suffix="ADA"
+            data={tvlData}
+            hasFilter
+          />
+          <ChartWidget
+            title="Community Revenue"
+            amount={4567}
+            suffix="ADA"
+            data={tvlData}
+          />
         </div>
-        <ChartWidget
-          title="Protocol Volume"
-          amount={4567}
-          suffix="ADA"
-          data={tvlData}
-          hasFilter
+        <Widget
+          noPrice
+          noHeaderPrice
+          titleLg={!balanceCBtc && !walletMeta}
+          title="Your cBTC"
+          walletMeta={walletMeta}
+          walletBalance={balanceCBtc}
+          onWalletBtnClick={handleWalletShowing}
+          token="cBTC"
+          icon="/images/crypto/cbtc-logo.svg#Layer_1"
         />
-        <ChartWidget
-          title="Community Revenue"
-          amount={4567}
-          suffix="ADA"
-          data={tvlData}
+        <Widget
+          dailyChangePrice={dailyChangeBtcPrice}
+          adaPrice={adaBtcPrice}
+          usdPrice={usdBtcPrice}
+          token="BTC"
+          icon={"/images/crypto/bitcoin-logo.svg#Layer_1"}
         />
-      </div>
-      <Widget
-        noPrice
-        noHeaderPrice
-        titleLg={!balanceCBtc && !walletMeta}
-        title="Your cBTC"
-        walletMeta={walletMeta}
-        walletBalance={balanceCBtc}
-        onWalletBtnClick={handleWalletShowing}
-        token="cBTC"
-        icon="/images/crypto/cbtc-logo.svg#Layer_1"
-      />
-      <Widget
-        dailyChangePrice={dailyChangeBtcPrice}
-        adaPrice={adaBtcPrice}
-        usdPrice={usdBtcPrice}
-        token="BTC"
-        icon={"/images/crypto/bitcoin-logo.svg#Layer_1"}
-      />
-      <Widget
-        adaPrice={adaFundPrice}
-        usdPrice={usdFundPrice}
-        title="Community Fund"
-      />
-      <Widget
-        title="Stake cNETA"
-        buttonTitle="Stake"
-        buttonLink="/stake"
-        noPrice
-        noHeaderPrice
-        titleLg
-      />
-      <Widget
-        adaValue="12"
-        title="Your Estimated Rewards"
-        buttonTitle="Claim Rewards"
-        buttonLink="https://app.tosidrop.io/cardano/claim"
-        externalLink
-        noPrice
-        noMargin
-      />
-      <Widget
-        noPrice
-        noMargin
-        adaValue="1,234"
-        title="Total cNETA Staked"
-        adaValue2={balanceCNeta ?? "--"}
-        title2="Your cNETA Staked"
-      />
-      <Widget title="Next Claiming Period" noPrice timer="2024/03/05" />
-      <Widget
-        title="Mint cBTC"
-        buttonTitle="Mint"
-        buttonLink="/"
-        noPrice
-        noHeaderPrice
-        titleLg
-      />
-      <ConnectWallet isOpen={isWalletShowing} setIsOpen={setIsWalletShowing} />
-      {/* <div className={styles.sectionBalance}>
+        <Widget
+          adaPrice={adaFundPrice}
+          usdPrice={usdFundPrice}
+          title="Community Fund"
+        />
+        <Widget
+          title="Stake cNETA"
+          buttonTitle="Stake"
+          buttonLink="/stake"
+          noPrice
+          noHeaderPrice
+          titleLg
+        />
+        <Widget
+          adaValue="12"
+          title="Your Estimated Rewards"
+          buttonTitle="Claim Rewards"
+          buttonLink="https://app.tosidrop.io/cardano/claim"
+          externalLink
+          noPrice
+          noMargin
+        />
+        <Widget
+          noPrice
+          noMargin
+          adaValue="1,234"
+          title="Total cNETA Staked"
+          adaValue2={balanceCNeta ?? "--"}
+          title2="Your cNETA Staked"
+        />
+        <Widget title="Next Claiming Period" noPrice timer="2024/03/05" />
+        <Widget
+          title="Mint cBTC"
+          buttonTitle="Mint"
+          buttonLink="/"
+          noPrice
+          noHeaderPrice
+          titleLg
+        />
+        <ConnectWallet
+          isOpen={isWalletShowing}
+          setIsOpen={setIsWalletShowing}
+        />
+        {/* <div className={styles.sectionBalance}>
         <div className={styles.balanceGroup}>
           <div className={styles.balance}>
             <h3 className={styles.balanceTitle}>Your cBTC</h3>
@@ -365,7 +373,8 @@ export default function Dashboard() {
           </Link>
         </div>
       </div> */}
-    </section>
+      </section>
+    </>
   );
 }
 
