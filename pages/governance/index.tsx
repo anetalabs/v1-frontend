2366ed30200d966cd56d34e5cb94fbc0bf1d9af1
tronexis <tronexis>
2024-03-07 20:17:02 +0600

    🚚 isolate OptionCard component

--- pages/governance/index.tsx
+++ pages/governance/index.tsx
@@ -1,15 +1,14 @@
 import styles from "../../styles/governance.module.scss";
-import Image, { StaticImageData } from "next/image";
-import { ChangeEvent, useContext, useEffect, useState } from "react";
-import { NumericFormat, numericFormatter } from "react-number-format";
+import Image from "next/image";
+import { useContext, useEffect, useState } from "react";
 import Head from "next/head";
 import cardano from "../../public/images/crypto/Cardano.svg";
 import cardanoBlack from "../../public/images/crypto/CardanoBlack.svg";
-import copy from "../../public/images/crypto/Cardano.svg";
 import { AppContext } from "../_app";
 import { numberFormat } from "../../utils/format";
 import tosidropLogo from "../../public/images/crypto/tosidrop-logo.png";
 import Link from "next/link";
+import OptionCard from "../../components/governance/OptionCard";
 
 export default function Governance() {
   return (
@@ -26,7 +25,6 @@ export default function Governance() {
         <OptionCard
           title="Option 1"
           description="Allocate 0.75 cBTC to cNETA staking and 0.5 cBTC to development."
-          walletLogo={cardano}
           walletName="Option 1 Cardano Wallet"
           walletAddress="addr1q80md4t6xacfxzvm5ak903gmmed0he3d0k3x4ylxwcvy3qs5jtjeq8d8xnq59jx4c9yrt9xr6xsn0srmdy2ghgtth72qsat083"
           votes="123456"
@@ -34,7 +32,6 @@ export default function Governance() {
         <OptionCard
           title="Option 2"
           description="Don’t enact these allocations at this time."
-          walletLogo={cardano}
           walletName="Option 2 Cardano Wallet"
           walletAddress="addr1q80md4t6xacfxzvm5ak903gmmed0he3d0k3x4ylxwcvy3qs5jtjeq8d8xnq59jx4c9yrt9xr6xsn0srmdy2ghgtth72qsat083"
           votes="123456"
@@ -60,84 +57,3 @@ export default function Governance() {
     </>
   );
 }
-
-interface OptionCardProps {
-  title: string;
-  description: string;
-  walletLogo: StaticImageData;
-  walletName: string;
-  walletAddress: string;
-  votes: string;
-}
-
-const OptionCard = (props: OptionCardProps) => {
-  const appContext = useContext(AppContext);
-  const { state } = appContext ?? { state: null };
-  const [copied, setCopied] = useState(false);
-
-  const handleCopyClick = () => {
-    setCopied(true);
-  };
-
-  useEffect(() => {
-    const copyToClipboard = (str: string) => {
-      navigator.clipboard.writeText(str);
-    };
-
-    if (copied) {
-      copyToClipboard(props.walletAddress);
-      const timeout = setTimeout(() => {
-        setCopied(false);
-      }, 1500);
-      return () => clearTimeout(timeout);
-    }
-  }, [copied, props.walletAddress]);
-
-  // Clear the timeout if the component is unmounted before it triggers
-
-  return (
-    <div className={styles.card}>
-      <h2>{props.title}</h2>
-      <p className={styles.description}>{props.description}</p>
-      <Image
-        src={state?.darkMode ? cardano : cardanoBlack}
-        className={styles.walletLogo}
-        alt={`${props.walletName} logo`}
-        width={120}
-        height={30}
-      />
-      <p className={styles.walletName}>{props.walletName}</p>
-      <div className={styles.address}>
-        <p className={styles.addressText}>{props.walletAddress}</p>
-        <button
-          className={styles.addressCopyBtn}
-          onClick={handleCopyClick}
-          disabled={copied}
-        >
-          {copied ? (
-            "Copied!"
-          ) : state?.darkMode ? (
-            <Image
-              src="/images/icons/copy-dark.png"
-              width={14}
-              height={14}
-              alt="copy"
-              className={styles.icon}
-            />
-          ) : (
-            <Image
-              src="/images/icons/copy-light.png"
-              width={14}
-              height={14}
-              alt="copy"
-              className={styles.icon}
-            />
-          )}
-        </button>
-      </div>
-      <div className={styles.votes}>
-        <span>Current Votes: {numberFormat(props.votes)}</span>
-      </div>
-    </div>
-  );
-};
