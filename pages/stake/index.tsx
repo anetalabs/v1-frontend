80f67ac1754575e57666cbddadff5e7346a8e337
tronexis <tronexis>
2024-03-18 01:22:37 +0600

    ♻️ update staking ui

--- pages/stake/index.tsx
+++ pages/stake/index.tsx
@@ -1,25 +1,35 @@
 import styles from "../../styles/stake.module.scss";
-import Image from "next/image";
-import { ChangeEvent, useState } from "react";
-import { NumericFormat, numericFormatter } from "react-number-format";
+import { useCallback, useState } from "react";
 import Head from "next/head";
+import ConnectWallet from "../../components/partials/navbar/ConnectWallet";
+import useCardanoWallet from "../../hooks/useCardanoWallet";
+import useStakingInfo from "../../hooks/useStakingInfo";
+import Link from "next/link";
+import { numberFormat } from "../../utils/format";
 
 export default function Stake() {
-  const monthlyRewards = 4567;
-  const maxAmount = 9999999;
-  const [amount, setAmount] = useState<number>(0);
+  const { walletMeta, address, walletAddress } = useCardanoWallet();
+  const [isWalletShowing, setIsWalletShowing] = useState(false);
+  const [stakingInfo, fetchStakingInfo] = useStakingInfo();
+  const [stakeLoading, setStakeLoading] = useState(false);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setAmount(parseFloat(value.replace(/,/g, "")));
-  };
+  const handleStake = useCallback(async () => {
+    setStakeLoading(true);
+    try {
+      const res = await fetch("api/stake");
+      const data = await res.json();
+      if (data.result === "ok") {
+        fetchStakingInfo();
+      }
+    } catch (error) {
+      console.error("Error fetching stake:", error);
+    }
+    setStakeLoading(false);
+  }, [fetchStakingInfo]);
 
-  const handleMaxClick = () => {
-    setAmount(maxAmount);
-  };
-
-  const handleStake = () => {
-    // TODO
+  const handleWalletShowing = () => {
+    if (isWalletShowing) setIsWalletShowing(false);
+    else setIsWalletShowing(true);
   };
 
   return (
@@ -28,48 +38,88 @@ export default function Stake() {
         <title>Staking | anetaBTC</title>
       </Head>
       <main className={styles.stake}>
-        <section>
-          <h2>Stake Amount</h2>
-          <div className={styles.inputRow}>
-            <Image
-              src="/images/logo/angel.png"
-              alt="logo aneta"
-              width={36}
-              height={36}
-              priority
-            />
-            <NumericFormat
-              thousandSeparator
-              allowNegative={false}
-              value={amount}
-              onChange={handleChange}
-              type="text"
-              placeholder="Enter stake amount"
-              required
-            />
-            <button onClick={handleMaxClick}>Max</button>
+        {!stakingInfo ? (
+          <div className={styles.loaderChart}>
+            <div className={styles.loader}></div>
           </div>
-          <div className={styles.infoRow}>
-            <svg height={20} width={20} className={styles.icon}>
-              <use href="/images/icons/coins.svg#icon"></use>
-            </svg>
-            <p className={styles.description}>
-              Your Estimated Monthly Rewards Valued in ADA*
-            </p>
-            <p className={styles.amount}>
-              {numericFormatter(
-                monthlyRewards.toLocaleString("en-US", {
-                  maximumFractionDigits: 2,
-                }),
-                {}
-              )}{" "}
-              ADA
-            </p>
-          </div>
-        </section>
-        <button onClick={handleStake}>Stake</button>
-        <div className={styles.comingSoon}>Coming Soon</div>
+        ) : (
+          <>
+            <section>
+              {!walletMeta && <p>Connect your wallet to stake.</p>}
+              {walletMeta && !stakingInfo?.staking && (
+                <p>
+                  You are currently holding{" "}
+                  <b>
+                    {numberFormat(stakingInfo?.stake.toString() ?? "0", 5)}{" "}
+                    cNETA.
+                  </b>
+                  <br />
+                  Stake it to your earn share of 0.75 cBTC and 18,000 rsERG.
+                  Rewards are distributed each epoch. Once you are staked,
+                  additionally accumulated cNETA in your wallet will be
+                  automatically staked.{" "}
+                </p>
+              )}
+              {walletMeta && stakingInfo?.staking && (
+                <p>
+                  You are now staking{" "}
+                  <b>
+                    {numberFormat(stakingInfo?.liveStake.toString() ?? "0", 5)}{" "}
+                    cNETA.
+                  </b>
+                  <br />
+                  Track all staking details in our dashboard.
+                </p>
+              )}
+              {!stakingInfo?.staking ||
+                (!walletMeta && (
+                  <div className={styles.infoRow}>
+                    <p className={styles.description}>
+                      Your Estimated Rewards Each Epoch
+                      <svg height={16} width={16} className={styles.icon}>
+                        <use href="/images/icons/coins.svg#icon"></use>
+                      </svg>
+                    </p>
+                    <div className={styles.amount}>
+                      <p>
+                        {numberFormat(
+                          stakingInfo?.rewards.btc.toString() ?? "0",
+                          5
+                        )}{" "}
+                        cBTC
+                      </p>
+                      <p>
+                        {numberFormat(
+                          stakingInfo?.rewards.erg.toString() ?? "0",
+                          5
+                        )}{" "}
+                        rsERG
+                      </p>
+                    </div>
+                  </div>
+                ))}
+            </section>
+            {!walletMeta && (
+              <button onClick={handleWalletShowing}>Connect Wallet</button>
+            )}
+            {walletMeta && !stakingInfo?.staking && (
+              <button
+                onClick={handleStake}
+                disabled={!stakingInfo || stakeLoading}
+              >
+                Stake
+              </button>
+            )}
+            {walletMeta && stakingInfo?.staking && (
+              <Link href="/dashboard" className={styles.button}>
+                Dashboard
+              </Link>
+            )}
+            {/* <div className={styles.comingSoon}>Coming Soon</div> */}
+          </>
+        )}
       </main>
+      <ConnectWallet isOpen={isWalletShowing} setIsOpen={setIsWalletShowing} />
     </>
   );
 }
