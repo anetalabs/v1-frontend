bb0036c37fdee94f32f54c722bb3820e52b3fbf2
tronexis <tronexis>
2024-03-25 06:23:36 +0600

    ✨ add tooltip

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -239,7 +239,7 @@ export default function Dashboard() {
               : "0 cNETA"
           }
           title2={walletMeta ? "Your cNETA Staked" : undefined}
-          // text2="Coming Soon"
+          title2Tooltip="Staked cNETA becomes active after 1 full epoch staked. If you stake during the 1st epoch, it becomes live in the 2nd epoch and rewards become available at the start of the 3rd epoch."
           text2={
             !walletMeta
               ? undefined
@@ -364,6 +364,7 @@ export default function Dashboard() {
               : undefined
           }
           buttonLink="/stake"
+          // tooltip="Staked cNETA becomes active after 1 full epoch staked. If you stake during the 1st epoch, it becomes live in the 2nd epoch and rewards become available at the start of the 3rd epoch."
           noPrice
           noHeaderPrice
           titleLg
