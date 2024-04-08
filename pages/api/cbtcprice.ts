784b014844f721a9fe70f31a61a2a8000795ed1b
tronexis <tanjilahmed7575@gmail.com>
2024-04-08 23:43:25 +0600

    👽 change cbtc price api (#54)
    * 📱 fix tooltip position in mobile view
    
    * ✨ add cbtc, cneta and ada price widgets
    
    * 👽 change cbtc price api
    
    * 🗑️ remove unused console log
    
    ---------
    
    Co-authored-by: tronexis <tronexis>

--- pages/api/cbtcprice.ts
+++ pages/api/cbtcprice.ts
@@ -1,19 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const URL = "https://api.poloniex.com/markets/CBTC_USDT/price";
+const API_KEY = process.env.CMC_API_KEY ?? "";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  const URL = `https://pro-api.coinmarketcap.com/v4/dex/pairs/quotes/latest?network_slug=cardano&contract_address=333f12dc7aed82cd3d4c057e28859fadfd677fc93e0662ab4be5942c9e194dc2`;
+
   try {
-    if (req.method !== "GET") {
-      return res.status(405).end();
-    }
+    const result = await (
+      await fetch(URL, {
+        headers: {
+          "X-CMC_PRO_API_KEY": API_KEY,
+        },
+      })
+    ).json();
 
-    const fetchResponse = await (await fetch(`${URL}`, {})).json();
-
-    res.status(200).send(fetchResponse);
+    res.status(200).send({
+      price: 1 / result.data[0].quote[0].price_by_quote_asset,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "server error" });
