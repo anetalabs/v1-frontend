b9a84faa666da8b37e35e92bef444dd0bbfe79de
tronexis <tronexis>
2024-05-30 00:58:31 +0600

    ♻️ update voting options

--- pages/governance/index.tsx
+++ pages/governance/index.tsx
@@ -9,48 +9,49 @@ import useVoting from "../../hooks/useVoting";
 export default function Governance() {
   const votingInfo = useVoting();
   return (
-    <>
-      <Head>
-        <title>Governance | anetaBTC</title>
-      </Head>
-      <main className={styles.governance}>
-        <p className={styles.text}>
-          Send your cVOTE7 tokens to the wallet you would like to vote for. At
-          the end of the 72-hour voting period, the option with the most votes
-          will be the outcome of the governance event.
-        </p>
-        <OptionCard
-          title="Option 1"
-          description="Implement Multisig Vault upgrade by May 1st, 2024."
-          walletName="Option 1 Cardano Wallet"
-          walletAddress="addr1qy8cmq0qw8dj7zeyfezp9vwfgjyv3tu8e7a82qe4fvr8dxwpsmwhh9rxntndhz93m944fsh0s9y725fr0as26y0wkceq9pyx90"
-          votes={votingInfo?.voteYesBalance ?? 0}
-        />
-        <OptionCard
-          title="Option 2"
-          description="Don’t enact the Mutlisig Vault Upgrade at this time."
-          walletName="Option 2 Cardano Wallet"
-          walletAddress="addr1q843md2ar09mervl060ypmhzx43ftnrqzymz4pk4rr5clcrr2jt3ptrul97g7fzk3w7dkv7rkw5mej7jr65x4v3xqqns5yepxp"
-          votes={votingInfo?.voteNoBalance ?? 0}
-        />
-        <Link
-          href="https://app.tosidrop.io/cardano/claim"
-          target="_blank"
-          className={styles.button}
-        >
-          Claim cVOTE7 tokens on{" "}
-          <Image
-            src={tosidropLogo}
-            alt="tosidrop logo"
-            width={120}
-            height={36}
-            className={styles.tosidropLogo}
-          />{" "}
-          <svg width="25" height="25" id="">
-            <use href={"/images/icons/right-arrow.svg#icon"}></use>
-          </svg>
-        </Link>
-      </main>
-    </>
-  );
+			<>
+				<Head>
+					<title>Governance | anetaBTC</title>
+				</Head>
+				<main className={styles.governance}>
+					<p className={styles.text}>
+						Send your cVOTE8 tokens to the wallet you would like to vote for. At
+						the end of the 72-hour voting period, the option with the most votes
+						will be the outcome of the governance event.
+					</p>
+					<OptionCard
+						title="Option 1"
+						description="Remove fees for 1 month."
+						walletName="Option 1 Cardano Wallet"
+						walletAddress="addr1qy8cmq0qw8dj7zeyfezp9vwfgjyv3tu8e7a82qe4fvr8dxwpsmwhh9rxntndhz93m944fsh0s9y725fr0as26y0wkceq9pyx90"
+						votes={votingInfo?.voteYesBalance ?? 0}
+					/>
+					<OptionCard
+						title="Option 2"
+						description="Don't change the fee structure at this time."
+						walletName="Option 2 Cardano Wallet"
+						walletAddress="addr1q843md2ar09mervl060ypmhzx43ftnrqzymz4pk4rr5clcrr2jt3ptrul97g7fzk3w7dkv7rkw5mej7jr65x4v3xqqns5yepxp"
+						votes={votingInfo?.voteNoBalance ?? 0}
+					/>
+					<Link
+						href="https://app.tosidrop.io/cardano/claim"
+						target="_blank"
+						className={styles.button}
+					>
+						Claim cVOTE8 tokens on{" "}
+						<Image
+							src={tosidropLogo}
+							alt="tosidrop logo"
+							width={120}
+							height={36}
+							className={styles.tosidropLogo}
+						/>{" "}
+						<svg width="25" height="25" id="">
+							<title>Right Arrow icon</title>
+							<use href={"/images/icons/right-arrow.svg#icon"} />
+						</svg>
+					</Link>
+				</main>
+			</>
+		);
 }
